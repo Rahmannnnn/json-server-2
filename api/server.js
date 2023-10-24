d25f7d711d5bee128b0477526e2505aa65977b7f
arif.ghani <arif.ghani@shopee.com>
2023-10-24 17:06:35 +0700

    initial

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
@@ -22,6 +23,9 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+
+server.use(auth)
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
