bc15b0b1ba8f2396a04cd64279015e8a9ac0408a
arif.ghani <arif.ghani@shopee.com>
2023-10-24 17:09:18 +0700

    back to initial

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const auth = require('json-server-auth')
 
 const server = jsonServer.create()
 
@@ -23,9 +22,6 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-server.use(auth)
-
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
