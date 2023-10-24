859997080ec12c25a0e2d0c175c636cc49f64181
arif.ghani <arif.ghani@shopee.com>
2023-10-24 17:43:35 +0700

    add router.db

--- api/server.js
+++ api/server.js
@@ -16,13 +16,9 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
+server.db = router.db
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
 server.use(auth)
 server.use(router)
 server.listen(3000, () => {
